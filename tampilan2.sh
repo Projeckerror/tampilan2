62e1f63623271e1d0a6a318782da4cd3a704fecf
Projeckerror <142165314+Projeckerror@users.noreply.github.com>
2023-08-31 19:19:01 +0700

    Add files via upload

---
+++ tampilan2.sh
@@ -0,0 +1,34 @@
+#Author : ABIDIN 25
+#Github : ABIDIN 25
+#Youtube
+clear
+#warna
+m="\033[1;31m"
+h="\033[1;32m"
+k="\033[1;33m"
+b="\033[1;34m"
+p="\033[1;37m"
+sleep 2.1
+toilet -f big -F  gay LOGIN
+toilet -f big -F  gay Bug Hunter
+mpv /storage/emulated/0/Music
+clear
+sleep 2
+toilet -f big -F  gay Hacker
+toilet -f big -F  gay Indonesia
+echo -e $p
+echo '================================================================'
+echo -e $h '                 welcome to the world of hackers'   $p
+echo '================================================================'
+echo
+echo         '╔══════════════════════════════════════════════════════════════╗'
+echo -e $h'║                   [•] Author : Panglima Jatim                ║' $k
+echo -e $k'║                   [•] Time   : Mafia Teknologi               ║' $p
+echo         '╚══════════════════════════════════════════════════════════════╝'
+echo -e $m
+date
+echo ''
+PS1='^[[1;36m╭^[[1;37m[^[[1;32mDATA^[[1;37m]^[[1;36m—^[[1;37m[^[[1;33m\d^[[1;37m]^[[1;36m—^[[1;37m[^[[1;35m \w ^[[1;37m]
+^[[1;36m╰───^[[1;33m▶ ^[[1;37m'
+
+
